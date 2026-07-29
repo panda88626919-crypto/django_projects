ec0c1e95d9a5d3929f493798bd5efc092749ae17
Maya Waleed <panda88626919@gmail.com>
2026-07-29 03:05:54 +0200

    Fix posts view

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
 def post_list(request):
-    return render(request, 'posts/post_list.html')    
+    return render(request, 'posts/posts_list.html')
